5044215d3924b07d7250137f00e9a88f592bcc45
= <moibueno2011@hotmail.com>
2022-07-29 22:46:24 -0300

    implementcao prototype

---
+++ creational-patterns/Prototype/IdInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber){
+            this.IdNumber = idNumber;
+        }
+    }
+}

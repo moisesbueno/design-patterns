b138993a0e7f471e0eb50165674f56346cf0734c
= <moibueno2011@hotmail.com>
2022-09-10 09:07:19 -0300

    state implementation

---
+++ behavioral-patterns/State/ConcreteStateA.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace State
+{
+    public class ConcreteStateA : State
+    {
+        public override void Handle1()
+        {
+            Console.WriteLine("ConcreteStateA handles request1.");
+            Console.WriteLine("ConcreteStateA wants to change the state of the context.");
+            this._context.TransitionTo(new ConcreteStateB());
+        }
+
+        public override void Handle2()
+        {
+            Console.WriteLine("ConcreteStateA handles request2.");
+        }
+    }
+}
---
+++ behavioral-patterns/State/Program.cs
@@ -0,0 +1,5 @@
+﻿using State;
+
+var context = new Context(new ConcreteStateA());
+context.Request1();
+context.Request2();
